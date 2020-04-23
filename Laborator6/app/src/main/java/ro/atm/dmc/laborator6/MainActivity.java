47f113461964eac9da4e7b8dab4ebaf248833ff5
Catalin <catalin.boja@hotmail.com>
2020-04-23 13:04:00 +0300

    Laborator 6 partial update

--- Laborator6/app/src/main/java/ro/atm/dmc/laborator6/MainActivity.java
+++ Laborator6/app/src/main/java/ro/atm/dmc/laborator6/MainActivity.java
@@ -1,8 +1,14 @@
 package ro.atm.dmc.laborator6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,10 +16,15 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import ro.atm.dmc.laborator6.modele.ToDo;
+import ro.atm.dmc.laborator6.modele.ToDoAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MENU_ITEM_DELETE = 1;
+    private static final int MENU_ITEM_DETAILS = 2;
+
     ArrayList<ToDo> toDos = new ArrayList<>();
+    ToDoAdapter toDoAdapter;
 
     private void initListaToDo(){
         ToDo activitate1 =
@@ -38,8 +49,48 @@ public class MainActivity extends AppCompatActivity {
         //incarcam date in listView printr-un adaptor standard
         ArrayAdapter<ToDo> toDoArrayAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,toDos);
-        ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
-        listViewToDo.setAdapter(toDoArrayAdapter);
 
+        //incarcam date in ListView prin adaptorul particular
+        toDoAdapter = new ToDoAdapter(toDos, this);
+
+
+
+        ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
+        //listViewToDo.setAdapter(toDoArrayAdapter);
+        listViewToDo.setAdapter(toDoAdapter);
+
+        //aduga meniul contextual la listView
+        registerForContextMenu(listViewToDo);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //adaugam meniul contextual pe fiecare element din listView/adaptor
+        AdapterView.AdapterContextMenuInfo  adapterContextMenuInfo =
+                (AdapterView.AdapterContextMenuInfo) menuInfo;
+        String titlu = ((ToDo)toDoAdapter.getItem(adapterContextMenuInfo.position)).getText();
+        menu.setHeaderTitle(titlu);
+
+        menu.add(Menu.NONE, MENU_ITEM_DELETE, 1,"Sterge");
+        menu.add(Menu.NONE, MENU_ITEM_DETAILS, 1,"Detalii");
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_ITEM_DELETE: {
+                AdapterView.AdapterContextMenuInfo adapterContextMenuInfo =
+                        (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                int idElementSelectat =
+                        adapterContextMenuInfo.position;
+                toDoAdapter.stergeElement(idElementSelectat);
+                return true;
+            }
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
---
+++ Laborator6/app/src/main/java/ro/atm/dmc/laborator6/modele/ToDoAdapter.java
@@ -0,0 +1,71 @@
+package ro.atm.dmc.laborator6.modele;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import ro.atm.dmc.laborator6.R;
+
+public class ToDoAdapter extends BaseAdapter {
+
+    List<ToDo> toDoList;
+    Context context;
+
+    public ToDoAdapter(List<ToDo> toDoList, Context context) {
+        this.toDoList = toDoList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return toDoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return toDoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        //de returnat un id unic pentru fiecare element ToDo
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View viewToDo = layoutInflater.inflate(R.layout.todo_item_layout, parent, false);
+
+        ImageView iv = viewToDo.findViewById(R.id.imageView);
+        TextView textViewData = viewToDo.findViewById(R.id.text1);
+        TextView textViewText = viewToDo.findViewById(R.id.text2);
+
+        ToDo toDo = toDoList.get(position);
+        iv.setImageResource(toDo.getImagine());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        textViewData.setText(simpleDateFormat.format(toDo.getTermenLimita()));
+        textViewText.setText(toDo.getText());
+
+        //activare meniu contextual pe long click
+        viewToDo.setLongClickable(true);
+
+        return viewToDo;
+    }
+
+    public void stergeElement(int pozitie){
+        toDoList.remove(pozitie);
+        //notificare view cu privire la modificarea datelor
+        notifyDataSetChanged();
+    }
+
+
+}
